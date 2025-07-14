cd7965ec4ac9bf4d035509771f6c31bcb21bf4a9
mtshn <128024133+mtshn@users.noreply.github.com>
2025-07-14 07:38:56 +0300

    scripts + fixed problems with file locations

---
+++ scripts/linux/command_line_interface/examples_cli.sh
@@ -0,0 +1,8 @@
+mkdir out
+sh ./explain_cli.sh -O1 ./out/out1a.csv  -O ./out/out1b.csv --fileFormat MSP --prefix ../gchrmsexplain/example_spectra --inputFile ../gchrmsexplain/example_input/input.csv
+sh ./explain_cli.sh -O1 ./out/out2a.csv  -O ./out/out2b.csv --fileFormat CSV --csvSpectrumHeader "Scan Number,m/z,Intensity,Relative,Segment Number,Flags" --mzColumn 1 --intensColumn 3  --prefix ../gchrmsexplain/example_spectra --inputFile ../gchrmsexplain/example_input/input2.csv
+sh ./explain_cli.sh -O1 ./out/out3a.csv  -O ./out/out3b.csv --fileFormat MSP1 --inputFile ../gchrmsexplain/example_spectra/spectra.msp
+sh ./explain_cli.sh -O1 ./out/out4a.csv  -O ./out/out4b.csv --fileFormat SDF --inputFile ../gchrmsexplain/example_spectra/spectra.sdf
+sh ./explain_cli.sh -O1 ./out/out5a.csv  -O ./out/out5b.csv --fileFormat SDF --inputFile ../gchrmsexplain/example_spectra/spectra1.sdf
+cat ./out/*a.csv > ./out/a.csv
+
